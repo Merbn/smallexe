b723ca48e60ce85408c8b4a2c20c52d6e4e292fa
merbng <merbng@163.com>
2018-01-08 23:32:22 +0800

    detail commit

--- smallexe/pages/posts/post-detail/post-detail.js
+++ smallexe/pages/posts/post-detail/post-detail.js
@@ -1,1 +1,21 @@
-{}
+var postData = require('../../../data/posts-data.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var postId = options.id;
+    var postData = postData.postList[postId];
+    this.data.postData = postData;
+
+  },
+})
